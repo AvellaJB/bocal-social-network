04f3d3b762060e910f6ead13167d23735bb348fe
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-04-06 14:24:26 +0200

    clean des fichiers inutiles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,2 +1,10 @@
 //Demander simplemetn les identifiants de la personne.
 // Ajouter un liens vers le CreateProfil.
+
+import React from "react";
+
+function Login() {
+  return <div>Login</div>;
+}
+
+export default Login;
--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,1 +1,8 @@
 // Page appelée avec l'ID Utilisateurs et ses infos si il est connecté.
+import React from "react";
+
+function Profil() {
+  return <div>Profil</div>;
+}
+
+export default Profil;
